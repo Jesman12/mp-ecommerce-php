1c334123000b00e5ba0a6b643cbc19dbf469ec54
Jesús Manuel Cuervo Iturbide <jesman2012@gmail.com>
2023-02-24 11:40:18 -0600

    Cambios en Success

--- success.php
+++ success.php
@@ -1,3 +1,8 @@
 <?php
     print_r($_REQUEST);
+
+    $nombreArchivo = "datosSuccess.txt";
+    $archivo = fopen($nombreArchivo, "w");
+    fwrite($archivo, implode(",", $_REQUEST));
+    fclose($archivo);
 ?>
